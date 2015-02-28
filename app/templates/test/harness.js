b52712e2e4dd1ab6a98efcb8d31cb93451f1c0f9
Trevor Livingston <trlivingston@paypal.com>
2015-02-28 14:45:28 -0600

    code style and desc

--- app/templates/test/harness.js
+++ app/templates/test/harness.js
@@ -1,10 +1,9 @@
-import tape from 'tape';
-import glob from 'glob';
+import Glob from 'glob';
 import Path from 'path';
 
 setImmediate(() => {
     process.argv.slice(2).forEach(arg => {
-        glob.sync(arg).forEach(file => {
+        Glob.sync(arg).forEach(file => {
             require(Path.resolve(process.cwd(), file));
         });
     });
